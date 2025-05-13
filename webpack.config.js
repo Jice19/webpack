6cfd3ed1870a3f4bebedec1ceac8c841be8db9b0
Jice19 <2452881917@qq.com>
2025-05-13 23:51:19 +0800

    feat:使用html-webpack-plugin插件实现html文件分离

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // 压缩css
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// 分离html文件
+const HTMLPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // 入口
@@ -11,7 +13,8 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     // 使用Node方法输出
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './dist'),
+    clean: true, // 每次构建前清理dist目录
   },
   module: {
     rules: [
@@ -31,14 +34,20 @@ module.exports = {
     // 单独提取CSS
     new MiniCssExtractPlugin({
       filename: "main.css"
-    })
+    }),
+    // 配置HTML插件
+    new HTMLPlugin({
+      template: "./public/index.html", // 指定模板文件路径
+      filename: "index.html", // 输出的HTML文件名
+      inject: "body", // 将脚本注入到body底部
+    }),
   ],
   optimization: {
     minimizer: [
       // 使用内置的TerserPlugin压缩JS（Webpack 5默认包含）
       `...`, 
       // 使用css-minimizer-webpack-plugin压缩CSS
-      new CssMinimizerPlugin()
+      new CssMinimizerPlugin(),
     ],
   },
 };
