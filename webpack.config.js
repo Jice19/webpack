92db748af4d7343fd2e72974de572f44bd923d3d
Jice19 <2452881917@qq.com>
2025-05-13 23:38:06 +0800

    feat:通过npm install --sace-dev mini-css-extract-plugin安装插件并进行一些配置实现css分离

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+// 分离操作
+const MiniCssExtraPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   // 入口
@@ -12,11 +14,28 @@ module.exports = {
   module:{
     rules:[
       // 配置很多处理方案：css、es6、vue、react
+      // {
+      //   // 相当于src：找对应匹配文件
+      //   test:/\.css$/,
+      //   use:['style-loader','css-loader']
+      // }
+
       {
         // 相当于src：找对应匹配文件
         test:/\.css$/,
-        use:['style-loader','css-loader']
+        use:[
+          {
+            loader:MiniCssExtraPlugin.loader
+          },
+          "css-loader"
+        ]
       }
     ]
-  }
+  },
+  plugins:[
+    // 单独提取css
+    new MiniCssExtraPlugin({
+      filename:"main.css"
+    })
+  ]
 }
