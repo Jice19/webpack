5be9f41221b850fe28ef14968941eec5449fd759
Jice19 <2452881917@qq.com>
2025-05-13 21:35:36 +0800

    feat:webpack不支持读取css文件，通过下载loader并配置webpack.config.json实现了解析css，通过配置package.json中的script实现了使用npm start 代替webpack指令

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+// 引入css文件
+require('./css/init.css')
+
 const show = require('./show.js')
 show('Jice')
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,15 @@ module.exports = {
     filename:'bundle.js',
     // 使用node方法输出
     path:path.resolve(__dirname, './dist')
+  },
+  module:{
+    rules:[
+      // 配置很多处理方案：css、es6、vue、react
+      {
+        // 相当于src：找对应匹配文件
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      }
+    ]
   }
 }
